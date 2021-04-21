8152d8e6be3efbe2e04867f469226e2f355eaf2a
StutiDid <77789710+StutiDid@users.noreply.github.com>
2021-04-21 20:57:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-document.getElementById ("myCanvas");
-ctx= canvas.getContext ("2d");
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.strokeStyle "grey";ctx.lineWidth = 1;
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
 ctx.rect(150, 143, 430, 200);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "blue";
 ctx.lineWidth = 5;
-ctx.arc (250, 210, 40 canvas ,0 , 2 * Math.PI);
+ctx.arc(250, 210, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
